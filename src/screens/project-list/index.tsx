8712e526c21a10ea1c3a9f09f739f4a73998701b
Thomas_huang <1424392205@qq.com>
2022-01-20 00:26:16 +0800

    chore: 给useDebounce增加泛型

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { cleanObject, useDebounce } from "../../utils";
+import * as qs from "qs";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([]);
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const debouncedParam = useDebounce(param, 2000);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List list={list} users={users} />
+    </div>
+  );
+};
---
+++ src/utils/index.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
+
+export const cleanObject = (object: object) => {
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    // @ts-ignore
+    const value = result[key];
+    if (isFalsy(value)) {
+      // @ts-ignore
+      delete result[key];
+    }
+  });
+  return result;
+};
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+export const useDebounce = <V>(value: V, delay?: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
+};
+
+const debounce = (func: () => void, delay?: number) => {
+  let timeout: number;
+  return (...param: any) => {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(function () {
+      // @ts-ignore
+      func(...param);
+    }, delay);
+  };
+};
