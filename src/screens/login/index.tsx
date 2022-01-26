cac548151035ad609e5d9615698d896d54268e46
Thomas_huang <1424392205@qq.com>
2022-01-27 00:57:17 +0800

    chore: 存储用户信息到全局

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,20 +1,22 @@
 import React, { FormEvent } from "react";
+import { useAuth } from "../../context/auth-context";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export const LoginScreen = () => {
-  const login = (param: { username: string; password: string }) => {
-    fetch(`${apiUrl}/login`, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(param),
-    }).then(async (response) => {
-      if (response.ok) {
-      }
-    });
-  };
+  // const login = (param: { username: string; password: string }) => {
+  //   fetch(`${apiUrl}/login`, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-type": "application/json",
+  //     },
+  //     body: JSON.stringify(param),
+  //   }).then(async (response) => {
+  //     if (response.ok) {
+  //     }
+  //   });
+  // };
+  const { login, register, user } = useAuth();
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const username = (e.currentTarget.elements[0] as HTMLFormElement).value;
